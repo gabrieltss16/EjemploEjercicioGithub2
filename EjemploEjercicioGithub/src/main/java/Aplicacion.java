a622804088ad99f1984fee32dc5d90a2e3e59e84
gabrieltss16 <gabriel.tomas.sifuentes@gregoriofer.com>
2025-04-08 22:38:33 +0200

    Update Aplicacion.java
    fvdabab

--- EjemploEjercicioGithub/src/main/java/Aplicacion.java
+++ EjemploEjercicioGithub/src/main/java/Aplicacion.java
@@ -4,6 +4,8 @@ public class Aplicacion {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("HOla mundo 222222222222222222222");
+		System.out.println("Hola mundo");
+		System.out.println("HOla mundo 222222222222222222222");
 	}
 
 }
